9ec9eae545e0662e7aaa7712e7242a2d5abbf643
nidhi00y <nidhiy2810@gmail.com>
2026-09-01 22:35:44 +0530

    Resume chat and new chat feature with sidebar

--- Chatbot/Chatbot_Ui.py
+++ Chatbot/Chatbot_Ui.py
@@ -1,12 +1,66 @@
 import streamlit as st
 from chatbot_backend import workflow
 from langchain_core.messages import BaseMessage, HumanMessage
+import uuid #for genarting dynamic thread_ids
+
+
+#*******************UTILITY FUNCTION********************************
+def generate_thread_id():
+     thread_id = uuid.uuid4()
+     return thread_id
+
+def reset_chat():
+     thread_id = generate_thread_id()
+     st.session_state['thread_id']=thread_id
+     add_thread(thread_id)
+     st.session_state['message_history'] = []
+
+def add_thread(thread_id):
+     if thread_id not in st.session_state['chat_thread_id']:
+          st.session_state['chat_thread_id'].append(thread_id)
+
+def load_conversation(thread_id):
+     return workflow.get_state(config={"configurable": {"thread_id": thread_id}})["messages"]
 
 
 ## st.session_state ->dict 
 ##content doesnt refresh on enter, only when the site is refreshed
 if 'message_history' not in st.session_state:
      st.session_state['message_history'] = []
+
+if 'thread_id' not in st.session_state:
+     st.session_state['thread_id'] = generate_thread_id()
+
+if 'chat_thread_id' not in st.session_state:
+     st.session_state['chat_thread_id'] = []
+
+add_thread(st.session_state['thread_id'])
+     
+
+#*********************SIDE BAR****************************************
+st.sidebar.title('LangGarph Chatbot')
+
+if(st.sidebar.button('New Chat')):
+     reset_chat()
+
+st.sidebar.header('My Coversations')
+for c in st.session_state['chat_thread_id'][::-1]:
+     if st.sidebar.button(str(c)):
+          st.session_state['thread_id'] = c
+          messages = load_conversation(c)
+
+          temp_messages=[]
+          for m in messages:
+               if isinstance(m, HumanMessage):
+                    temp_messages.append({'role':'user','content':m.content})
+               else:
+                    temp_messages.append({'role':'assistant','content':m.content})
+
+          st.session_state['message_history'] = temp_messages
+
+
+
+#******************************MAIN UI*************************************
 
 for x in st.session_state['message_history']:
         with st.chat_message(x['role']):
@@ -19,7 +73,7 @@ if(user_input):
     with st.chat_message('user'):
        st.text(user_input)
 
-    thread_id = '1'
+    thread_id = st.session_state['thread_id']
     config = {"configurable": {"thread_id": thread_id}}
     ##Ai_response = workflow.invoke({"messages": [HumanMessage(content=user_input)]}, config=config)
     ##response = Ai_response["messages"][-1].content
